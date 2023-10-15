822aafe979be964b5ac4b57e66478c44e5b9e4a8
MANISH TOMAR <manishtomar00mt@gmail.com>
2023-10-15 19:55:22 +0530

    Number system method Added

---
+++ week2/decimalToBinaryMethod1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int decimalToBinaryMethod1(int n){
+    //Division Method
+    while(n>0){
+        int bit = n%2;
+        cout<<bit<<endl;
+        n=n/2;
+    }
+    return 0;
+}
+int main(){
+    int n;
+    cin>>n;
+    int binary = decimalToBinaryMethod1(n);
+}
