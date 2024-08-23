01e72a26bbe6b2afc9b1960516a19158b75e5d14
MANISH TOMAR <manishtomar00mt@gmail.com>
2024-08-23 20:40:13 +0530

    Find_SquareRootbyBS.cpp Adding floating values in final output

--- 2.Searching & Sorting/Find_SquareRootbyBS.cpp
+++ 2.Searching & Sorting/Find_SquareRootbyBS.cpp
@@ -37,5 +37,19 @@ int main(){
 
     int ans = findSqrt(n);
     cout<<"Ans is "<< ans << endl;
+
+    int precision;
+    cout<< "Enter the number of floating digits in precision" <<endl;
+    cin>> precision;
+
+    double step =0.1;
+    double finalAns = ans;
+    for(int i=0; i<precision; i++){
+        for(double j=finalAns; j*j<=n; j=j+step){
+            finalAns = j;
+        }
+        step = step/10;
+    }
+    cout<<"Final ans is: "<<finalAns << endl;
     return 0;
 }
