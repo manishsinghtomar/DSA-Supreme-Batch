55e9ffce306235e599ee67fbd907ba315c32fa47
Manish Singh Tomar <manishtomar00mt@gmail.com>
2024-09-01 04:55:46 +0530

    Divide_2_numbers_using_BS.cpp Updated

--- 2.Searching & Sorting/Divide_2_numbers_using_BS.cpp
+++ 2.Searching & Sorting/Divide_2_numbers_using_BS.cpp
@@ -40,6 +40,24 @@ int main(){
 
     int ans = solve(dividend, divisor);
     cout<< "Ans is " << ans << endl;
+
+    int precision;
+    cout<< "Enter the number of floating digits in precision" <<endl;
+    cin >> precision;
+
+    double step =0.1;
+    double finalAns = abs((double)ans);
+    for(int i=0; i<precision; i++){
+        for(double j=finalAns; j*abs(divisor)<=abs(dividend); j=j+step){
+            finalAns = j;
+        }
+        step = step/10;
+    }
+    if ((divisor<0 && dividend<0)|| (divisor>0 && dividend>0 ))
+        cout<<"Final ans is: "<<finalAns << endl;
+    else {
+        cout<<"Final ans is: "<<-finalAns << endl;;
+    }
     return 0;
 }
 
