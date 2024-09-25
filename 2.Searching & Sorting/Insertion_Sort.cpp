a41496abbbbff37657fa56d0701526f0584342d9
Manish Singh Tomar <manishtomar00mt@gmail.com>
2024-09-25 23:55:58 +0530

    Insertion_Sort.cpp Added

---
+++ 2.Searching & Sorting/Insertion_Sort.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> arr{10,1,7,6,14,9};
+    int n = arr.size();
+    
+    //insertion sort
+    for(int round = 1; round < n; round++) {
+        //Step 1 - fetch
+        int val = arr[round];
+        //Step2 Compare
+        for(int j=round-1;j>=0; j--) {
+            if(arr[j]>val){
+
+            }
+            else{
+                //
+            }
+        }
+    }
+
+
+
+    return 0;
+}
