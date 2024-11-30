6900b65e40394c27299616d9e9a93414769d1f5b
Manish Singh Tomar <manishtomar00mt@gmail.com>
2024-11-30 15:52:54 +0530

    practice1.cpp added

---
+++ 3.Char Arrays & Strings/practice1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    char name[100];
+    getline
+    cin>>name;
+    for(int i = 0; i<13;i++){
+        cout<<name[i];
+    }
+    return 0;
+}
