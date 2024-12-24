ecffb91cd6bbecdbcd6beb2861ec3b7a3a858b0e
bhanu291998 <100214047+bhanu291998@users.noreply.github.com>
2024-12-24 21:01:02 +0530

    oops_class1.cpp Added

--- 8.OOPs/oops_class1.cpp
+++ 8.OOPs/oops_class1.cpp
@@ -1,7 +1,49 @@
 #include<iostream>
 using namespace std;
+
+class Animal {
+};
+
+class Animal2{
+
+    int age;
+    int weight;
+    char ch;
+
+};
+
+class Animal3{
+//In C++, the default access modifier for class members is private
+
+public:
+// state or properties
+    int age;
+    string name;
+
+    //behaviour    - behaviour means functions and we often call them methods in java
+    void eat(){
+
+    }
+
+    void sleep(){
+
+    }
+
+};
+
 int main(){
-    int result = 2+3;
-    cout<<result;
+ cout<<"size of empty class: "<< sizeof(Animal) << endl; //empty class takes 1 byte memory
+ cout<<"size of empty class: "<< sizeof(Animal2) << endl;// it takes 12 bytes bcoz of padding concept 
+
+  //Object Creation
+
+  //Static
+  Animal3 ramesh;
+  
+  cout<< "Age of ramesh is: "<<ramesh.age <<endl;
+  cout<< "name of ramesh is: "<<ramesh.name <<endl;
+
+
+  // Dynamic Memory
     return 0;
 }
